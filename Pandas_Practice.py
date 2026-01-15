240475caa80b0fa2b2a3f31c34afba5d0908a7ac
Joseph Anderson <josephanderson7180@gmail.com>
2026-01-15 19:41:52 +0000

    595 solved

--- Pandas_Practice.py
+++ Pandas_Practice.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+### 595) Big Countries
+
+# time O(n) Bolean filter complexity
+# space O(n) Bolean Mask time complexity
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    df = world[(world.population >= 25000000) | (world.area >= 3000000)]
+
+    df = df[['name', 'population', 'area']]
+
+    return df
+
