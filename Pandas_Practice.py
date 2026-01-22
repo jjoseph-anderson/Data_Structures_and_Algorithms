9643154ed91ce28158b9bc3609c357e3ff18a1f6
Joseph Anderson <josephanderson7180@gmail.com>
2026-01-22 19:05:10 +0000

    178 setup

--- Pandas_Practice.py
+++ Pandas_Practice.py
@@ -254,4 +254,11 @@ def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) 
 
     return out
 
-print(department_highest_salary(employee, department))
+# print(department_highest_salary(employee, department))
+
+#### 178) Rank Scores
+
+data = { "id": [1, 2, 3, 4, 5, 6],
+         "score": [3.50, 3.65, 4.00, 3.85, 4.00, 3.65] }
+
+scores = pd.DataFrame(data)
