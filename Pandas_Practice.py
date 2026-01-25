fe3f5e04b4b2c60af0b92338b4f75569a32a52a5
Joseph Anderson <josephanderson7180@gmail.com>
2026-01-25 11:54:37 +0000

    2356 solved

--- Pandas_Practice.py
+++ Pandas_Practice.py
@@ -360,3 +360,12 @@ data = { 'teacher_id': [1, 1, 1, 2, 2, 2, 2],
          'subject_id': [2, 2, 3, 1, 2, 3, 4],
          'dept_id': [3, 4, 3, 1, 1, 1, 1] }
 teacher = pd.DataFrame(data)
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    df = teacher.groupby(by = 'teacher_id', as_index = False)['subject_id'].nunique()
+
+    df = df.rename(columns = {'subject_id':'cnt'})
+
+    return df
+
+print(count_unique_subjects(teacher))
